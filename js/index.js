ed2981cf7ef6c7df31a2a8b5bcb8af830152b000
tomoliveira18 <weverton.oliveira@facode.com.br>
2022-06-02 22:10:59 -0300

    first version

---
+++ js/index.js
@@ -0,0 +1,29 @@
+ScrollReveal({ reset: false, distance: "60px", duration: 900, delay: 25 });
+
+ScrollReveal().reveal(".servicos-title, .cards", {
+  delay: 25,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".image, .text", {
+  delay: 100,
+  origin: "left"
+})
+
+Atropos({
+  el: '.my-atropos',
+  // rest of parameters
+  shadow: false
+});
+
+Atropos({
+  el: '.my-atropos-1',
+  // rest of parameters
+  shadow: false
+});
+
+Atropos({
+  el: '.my-atropos-2',
+  // rest of parameters
+  shadow: false
+});
